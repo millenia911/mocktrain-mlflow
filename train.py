26be7abec192cbec50627bd0aab5e4723c30fcb8
Freddy Millenia <freddymyusuf@gmail.com>
2022-05-18 14:10:05 +0700

    initial commit

---
+++ train.py
@@ -0,0 +1,11 @@
+import mlflow
+import argparse
+
+parser = argparse.ArgumentParser(description="Training Script")
+parser.add_argument('--path', type=str, default=None)
+opt = parser.parse_args()
+
+mlflow.start_run()
+mlflow.log_param("name", 12)
+mlflow.log_param("path", opt.path)
+mlflow.end_run()
