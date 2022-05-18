d7155a228b88b0304f9c12f41b2f4591f4e711a8
Freddy Millenia <freddymyusuf@gmail.com>
2022-05-19 05:32:16 +0700

    init new branch

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import mlflow
 import argparse
 import os
 os.system('nvidia-smi')
+print("trigger from new branch")
 
 parser = argparse.ArgumentParser(description="Training Script")
 parser.add_argument('--path', type=str, default=None)
@@ -10,4 +11,5 @@ opt = parser.parse_args()
 mlflow.start_run()
 mlflow.log_param("name", 12)
 mlflow.log_param("path", opt.path)
+mlflow.log_param("newbranch", "True")
 mlflow.end_run()
