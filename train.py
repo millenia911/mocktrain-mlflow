8ae628310b1d0ccea17faabda0d0099c4e4f5056
Freddy Millenia <freddymyusuf@gmail.com>
2022-05-19 08:35:52 +0700

    add some utility

--- train.py
+++ train.py
@@ -1,7 +1,22 @@
 import mlflow
 import argparse
 import os
+from random import sample
+from shutil import copyfile
+
 os.system('nvidia-smi')
+os.system('./blob-data-extractor.sh')
+os.system('ls .')
+os.makedirs('images', exist_ok=True)
+images = os.listdir('./new_dataset/dataset_val')
+images = sample(images, 10)
+
+path = './new_dataset/dataset_val'
+new_path = '.images'
+for im in images:
+    src = os.path.join(path, im)
+    dst = os.path.join(new_path, im)
+    copyfile(src, dst)
 
 parser = argparse.ArgumentParser(description="Training Script")
 parser.add_argument('--path', type=str, default=None)
@@ -10,4 +25,5 @@ opt = parser.parse_args()
 mlflow.start_run()
 mlflow.log_param("name", 12)
 mlflow.log_param("path", opt.path)
+mlflow.log_artifact("./images", "logged_images/")
 mlflow.end_run()
